85f7ff19527a5926e8d0fe4a7aa9f5ed1a983b21
hjimene <hjimene@LS-WALC2026P10>
2023-10-27 09:24:32 -0400

    Add project files.

---
+++ 06LAB_Jimenez_hector.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+int main() {
+    string background = "images1/backgrounds/alebrije.png";
+    string foreground = "images1/characters/yoda.png";
+    Texture backgroundTex;
+
+    if (!backgroundTex.loadFromFile(background)) {
+        cout << "Couldn't Load Background Image" << endl;
+        exit(1);
+    }
+
+    Texture foregroundTex;
+
+    if (!foregroundTex.loadFromFile(foreground)) {
+        cout << "Couldn't Load Foreground Image" << endl;
+        exit(1);
+    }
+
+    Image backgroundImage;
+    backgroundImage = backgroundTex.copyToImage();
+
+    Image foregroundImage;
+    foregroundImage = foregroundTex.copyToImage();
+
+    Vector2u sz = backgroundImage.getSize();
+
+    for (int y = 0; y < sz.y; y++) {
+        for (int x = 0; x < sz.x; x++) {
+            // These two loops will run the code inside for each pixel in the background image
+            // You can access the current pixel at x, y like so:
+            Color example = foregroundImage.getPixel(x, y);
+            // Color objects store the individual channel values like example.r, example.g, and example.b
+        }
+    }
+
+    // By default, just show the foreground image
+    RenderWindow window(VideoMode(1024, 768), "Here's the Output");
+
+    Sprite sprite1;
+    Texture tex1;
+    tex1.loadFromImage(foregroundImage);
+    sprite1.setTexture(tex1);
+
+    window.clear();
+    window.draw(sprite1);
+    window.display();
+
+    while (true) {
+        
+    }
+
+    return 0;
+}
